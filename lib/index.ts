39859a781f3e0c81876cb2995a655868473ffa5e
Aleix Morgadas <aleixmorgadas@users.noreply.github.com>
2021-09-29 16:47:08 +0100

    exporting AggregateCommand

--- lib/index.ts
+++ lib/index.ts
@@ -6,6 +6,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+export * from './AggregateCommand';
 export * from './Command';
 export * from './PlainCommand';
 export * from './MosaicCommand';
